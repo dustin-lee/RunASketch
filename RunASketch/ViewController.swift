7a3cd47e61735b680112d990161d449069ba83b9
Daniel Kouba <koubaphotog@gmail.com>
2016-12-12 11:22:40 -0800

    Added route, sort of

--- RunASketch/ViewController.swift
+++ RunASketch/ViewController.swift
@@ -15,24 +15,124 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
+    var userAnnotations = [MKPointAnnotation]()
+
+    var userRoute = [MKRoute]()
+    
+    ////////////////////////////////////////
+    // Draw Routes between points
+    ////////////////////////////////////////
+    func createDirection(annotation1: MKPointAnnotation,annotation2: MKPointAnnotation){
+        
+        
+        // 2.
+        let sourceLocation = CLLocationCoordinate2D(latitude: annotation1.coordinate.latitude, longitude: annotation1.coordinate.longitude)
+        let destinationLocation = CLLocationCoordinate2D(latitude: annotation2.coordinate.latitude, longitude: annotation2.coordinate.longitude)
+        
+        // 3.
+        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+        
+        // 4.
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+        
+        //        // 5.
+        //        let sourceAnnotation = annotation1
+        //        let destinationAnnotation = annotation2
+        
+        
+        // 7.
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationMapItem
+        directionRequest.transportType = .walking
+        
+        // Calculate the direction
+        let directions = MKDirections(request: directionRequest)
+        
+        // 8.
+        directions.calculate {
+            (response, error) -> Void in
+            
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                
+                return
+            }
+            
+            let route = response.routes[0]
+            self.mapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
+            self.userRoute.append(route)
+            
+            
+            print("////////////////////////////////////")
+            print("this is our route length")
+//            print(route.steps)
+            for desc in 0...(route.steps.count - 1) {
+                print(route.steps[desc])
+            }
+            print("////////////////////////////////////")
+            
+            //let rect = route.polyline.boundingMapRect
+            //self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 4.0
+        
+        return renderer
+        
+    }
+    
+    
+    //END Draw Lines
+    ////////////////////////////////////////
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     ////////////////////////////////////////
-    // Create a Pin
+    //  Create New Pin
     ////////////////////////////////////////
     @IBAction func createNewPin(_ sender: UILongPressGestureRecognizer) {
         
+        if sender.state == .began {
+            let touchPoint = sender.location(in: self.mapView)
+            let touchMapCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchMapCoordinate
+            
+            mapView.addAnnotation(annotation)
+            if userAnnotations.count >= 1{
+                createDirection(annotation1: userAnnotations[userAnnotations.count-1], annotation2: annotation)
+            }
+            userAnnotations.append(annotation)
+            
+        }
         
-        let touchPoint = sender.location(in: self.mapView)
-        let touchMapCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
-        
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = touchMapCoordinate
-        
-        mapView.addAnnotation(annotation)
     }
-    // End Create Pin
     ////////////////////////////////////////
+    // END Create Pin
+    ////////////////////////////////////////
+    
+
     
     
     
@@ -43,8 +143,10 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         self.locationManager.delegate = self
         self.locationManager.requestWhenInUseAuthorization()
         
-        
         self.determineCurrentLocation()
+        
+        
+        mapView.delegate = self
         
     }
     
@@ -52,7 +154,6 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
     
     func determineCurrentLocation()
@@ -74,7 +175,7 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         
         // Call stopUpdatingLocation() to stop listening for location updates,
         // other wise this function will be called every time when user location changes.
-        //        manager.stopUpdatingLocation()
+        manager.stopUpdatingLocation()
         
         let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
@@ -86,6 +187,7 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         myAnnotation.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude);
         myAnnotation.title = "Current location"
         mapView.addAnnotation(myAnnotation)
+        userAnnotations.append(myAnnotation)
     }
     
     private func locationManager(manager: CLLocationManager, didFailWithError error: NSError)
